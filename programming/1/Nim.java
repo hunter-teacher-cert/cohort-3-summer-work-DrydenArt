b436d17f9d4ebeea87a97fb3458641daf73a7640
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-07-01 13:01:26 +0000

    created folders 0 and 1, created Nim

---
+++ programming/1/Nim.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Nim 
+{
+  public static void main(String[] args)
+  {
+    int stones = 12;
+    int stonesTaken;
+    Scanner input = new Scanner(System.in);
+    //loop until game ends
+    while (stones > 0) {
+      //prompt user input: user taking turn
+      System.out.println("Please enter a number from 1 - 3 stones");
+      stonesTaken = input.nextInt();//taking from the user I
+      
+
+        //calculate number of stones remaining, print
+      stones = stones - stonesTaken;
+      //also could do: stones -= stonesTaken;
+      System.out.println("There are " + stones + " left");
+      //check win
+      if(stones <= 0)
+      {
+        System.out.println("You win!");
+      }
+      
+       //machine turn
+      System.out.println("Computer's Turn:");
+      stonesTaken = (int) (Math.random()*3)+1;
+      //Math.random()(max - min +1) +min this calculates range
+      stones = stones - stonesTaken;
+      System.out.println("The Computer took " + stonesTaken);
+      //calculate number of stones remaining, print
+      System.out.println("There are " + stones + " left");
+      if(stones <= 0)
+      {
+        System.out.println("Computer Wins!");
+      }
+
+     
+
+      //check win
+      
+       
+      
+    }
+    
+  }
+}
