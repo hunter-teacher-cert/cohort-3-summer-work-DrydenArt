c06377e90eadaa12e0df4e145163d1c3e27c4a76
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-07-08 02:04:57 +0000

    made notes in Fences.java

--- programming/5/Fence.java
+++ programming/5/Fence.java
@@ -22,21 +22,21 @@ public class Fence
     if (n == 0) //establishes base case
     {
       return ""; //this will return a space, however the "" could be empty,too
-    }else if (n == 1) //another base case
+    }else if (n == 1) //another base case set to 1
     {
       return "|"; //when it gets to 1, it just returns the one post
     }else
     {
-      return fenceR(n-1) + "--|"; //everything else 
+      return fenceR(n-1) + "--|"; //everything else, this will return the values from 2 - 9
     }
     
     
-  }
+  }//end of method
 
 
   public static void main( String[] args )
   {
-
+  
     for( int i = 0; i < 10; i++ ) {
       System.out.println( i + "-post fences: " );
       System.out.println( fenceR(i) );
