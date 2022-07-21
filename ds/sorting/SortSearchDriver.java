750242bc4031742a616de6808ae76c5a4e87eae8
DrydenArt <45575196+DrydenArt@users.noreply.github.com>
2022-07-21 21:30:20 +0000

    updated formatting in SortSearch.java and SortSearchDriver.java

--- ds/sorting/SortSearchDriver.java
+++ ds/sorting/SortSearchDriver.java
@@ -9,33 +9,38 @@ public class SortSearchDriver {
       // Uncomment these to test part 1
 	
 	SortSearch ss = new SortSearch(20);
-	System.out.println("This is the random array: \n" + ss +"\n");
+	System.out.println("\nThis is the random array: \n" + ss +"\n");
 	
 		
 	// Uncomment these to test part 2
 
 	int i;
-	i = ss.findSmallestIndex(0);
-	System.out.println("This is findSmallestIndex: \n"+"ss["+ i +"] = "+ss.get(i)+" : \n" + ss +"\n");
+	i = ss.findSmallestIndex(0);//zero determines the index to start searching
+	System.out.println("This is findSmallestIndex (starting from index 0): \n"+"ss["+ i +"] = "+ss.get(i)+" : \n" + ss +"\n");
 
-	i = ss.findSmallestIndex(3);
-	System.out.println("This is findSmallestIndex: \n" +"ss["+ i + "] = "+ss.get(i)+" : " + ss);
+	i = ss.findSmallestIndex(15);
+	System.out.println("This is findSmallestIndex (starting from index 15): \n" +"ss["+ i + "] = "+ss.get(i)+" : \n" + ss);
 	
 	// Uncomment these to test part 3
 
-	//System.out.println(ss);
+	System.out.println("\nThis is the original random array:\n" +ss);
 	ss.sort();
-	System.out.println("\n" + "This is the array sorted: " +ss);
+	System.out.println("\n" + "This is the array sorted:\n" +ss);
 
-  System.out.println("\n" + "This is linearSearch looking for the index of value 3: " +ss.linearSearch(3) +"\n");
-      
+  //Linear Search
+  System.out.println("\n" + "This is linearSearch looking for the index of value 3: " +ss.linearSearch(3));
+  System.out.println("This is linearSearch looking for the index of value 8: " +ss.linearSearch(8));
+  System.out.println("This is linearSearch looking for the index of value 0: " +ss.linearSearch(0));
+
+
+  //Binary Search
   System.out.println("\n" +"This is binary search looking for the index of value 5: " + ss.binarySearch(5));
 
-  System.out.println("\n" +"This is binary search looking for the index of value 9: " + ss.binarySearch(9));
+  System.out.println("This is binary search looking for the index of value 9: " + ss.binarySearch(9));
 
-  System.out.println("\n" +"This is binary search looking for the index of value 12: " + ss.binarySearch(12));
+  System.out.println("This is binary search looking for the index of value 12: " + ss.binarySearch(12));
 
-  System.out.println("\n" +"This is binary search looking for the index of value 15: " + ss.binarySearch(15));
+  System.out.println("This is binary search looking for the index of value 15: " + ss.binarySearch(15));
       
   //this shows that sort is exponential, that the larger the array size, the longer it takes to run/compile
   int size = 1000;//ran it with 1000, 10000, and 100000 and compared the elapsed time
@@ -53,7 +58,7 @@ public class SortSearchDriver {
 
 	// System.out.println(ss);
 	elapsed = System.currentTimeMillis() - start;
-	System.out.println("Size: " + size + " Time: " + elapsed);
+	System.out.println("\nSize: " + size + " Time: " + elapsed);
 
 
 	      
